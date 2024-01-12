e037af91c1ff3bba33c0d79863079ba290eaaefa
Ameer Ghani <inahga@gmail.com>
2024-01-12 00:58:30 -0600

    nvml: gofmt

--- nvml/nvml_windows.go
+++ nvml/nvml_windows.go
@@ -3,15 +3,15 @@
 package nvml
 
 import (
-	"os"
 	"log"
+	"os"
 	"unsafe"
+
 	"golang.org/x/sys/windows"
 )
 
 const (
 	nvmlSuccess uintptr = 0
-
 )
 
 func init() {
@@ -32,7 +32,7 @@ func init() {
 	if err != nil {
 		log.Printf("energy monitoring unavailable")
 	} else {
-		useEnergy=true
+		useEnergy = true
 	}
 	err = getPower.Find()
 	if err != nil {
@@ -42,28 +42,28 @@ func init() {
 			return
 		}
 	}
-	r1,_,err := initNVML.Call()
+	r1, _, err := initNVML.Call()
 	if r1 != nvmlSuccess {
 		log.Printf("failed initializing: %v", err)
 		return
 	}
 	var deviceCount uint64
-	r1,_,err = countDevices.Call(uintptr(unsafe.Pointer(&deviceCount)))
+	r1, _, err = countDevices.Call(uintptr(unsafe.Pointer(&deviceCount)))
 	if r1 != nvmlSuccess {
 		log.Printf("failed counting devices: %v", err)
 		return
 	}
 	log.Printf("found %d devices", deviceCount)
-	for i := uint64(0); i < deviceCount; i ++ {
+	for i := uint64(0); i < deviceCount; i++ {
 		var device uintptr
-		r1,_,err = getDeviceHandleByIndex.Call(uintptr(i),uintptr(unsafe.Pointer(&device)))
+		r1, _, err = getDeviceHandleByIndex.Call(uintptr(i), uintptr(unsafe.Pointer(&device)))
 		if r1 != nvmlSuccess {
 			log.Printf("failed counting devices: %v", err)
 			return
 		}
 		log.Printf("Loaded device %v", device)
 		var arch uint32
-		r1,_,err = getArchitecture.Call(uintptr(unsafe.Pointer(&device)), uintptr(unsafe.Pointer(&arch)))
+		r1, _, err = getArchitecture.Call(uintptr(unsafe.Pointer(&device)), uintptr(unsafe.Pointer(&arch)))
 		if r1 != nvmlSuccess {
 			log.Printf("failed getting device architecture: %v", err)
 			return
@@ -71,7 +71,7 @@ func init() {
 		log.Printf("Device architecture: %v", arch)
 		if useEnergy {
 			var uJ uint64
-			r1,_,err = getEnergy.Call(uintptr(unsafe.Pointer(&device)), uintptr(unsafe.Pointer(&uJ)))
+			r1, _, err = getEnergy.Call(uintptr(unsafe.Pointer(&device)), uintptr(unsafe.Pointer(&uJ)))
 			if r1 != nvmlSuccess {
 				log.Printf("failed reading energy: %v", err)
 				return
@@ -79,7 +79,7 @@ func init() {
 			log.Printf("Read %d uJ", uJ)
 		} else {
 			var mW uint32
-			r1,_, err = getPower.Call(uintptr(unsafe.Pointer(&device)), uintptr(unsafe.Pointer(&mW)))
+			r1, _, err = getPower.Call(uintptr(unsafe.Pointer(&device)), uintptr(unsafe.Pointer(&mW)))
 			if r1 != nvmlSuccess {
 				log.Printf("failed reading power: %v", err)
 			}
