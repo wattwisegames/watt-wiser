451cb53b9bcdcc98401fefdce44626ecfff640d6
Chris Waldon <christopher.waldon.dev@gmail.com>
2024-01-14 10:52:34 -0500

    nvml: remove map lookup from function call path
    This is a small optimization that just removes some work from invoking one of
    these dynamic functions.
    
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- nvml/nvml_windows.go
+++ nvml/nvml_windows.go
@@ -48,69 +48,75 @@ func platformInit() error {
 	}
 
 	// Build wrappers for required symbols
+	initFunc := resolved[symbolNvmlInit_v2]
 	nvmlInit = func() error {
-		rc, _, _ := resolved[symbolNvmlInit_v2].Call()
+		rc, _, _ := initFunc.Call()
 		if rc := nvmlError(rc); rc != NVML_SUCCESS {
 			return rc
 		}
 		return nil
 	}
+	getVersionFunc := resolved[symbolNvmlSystemGetNVMLVersion]
 	nvmlSystemGetNVMLVersion = func() (string, error) {
 		var version [16]byte
-		rc, _, _ := resolved[symbolNvmlSystemGetNVMLVersion].Call(uintptr(unsafe.Pointer(&version)), uintptr(len(version)))
+		rc, _, _ := getVersionFunc.Call(uintptr(unsafe.Pointer(&version)), uintptr(len(version)))
 		if rc := nvmlError(rc); rc != NVML_SUCCESS {
 			return "", rc
 		}
 		return strings.ReplaceAll(string(version[:]), "\000", ""), nil
 	}
+	getCountFunc := resolved[symbolNvmlDeviceGetCount_v2]
 	nvmlDeviceGetCount = func() (uint64, error) {
 		var count uint64
-		rc, _, _ := resolved[symbolNvmlDeviceGetCount_v2].Call(uintptr(unsafe.Pointer(&count)))
+		rc, _, _ := getCountFunc.Call(uintptr(unsafe.Pointer(&count)))
 		if rc := nvmlError(rc); rc != NVML_SUCCESS {
 			return 0, rc
 		}
 		return count, nil
 
 	}
+	getHandleFunc := resolved[symbolNvmlDeviceGetHandleByIndex_v2]
 	nvmlDeviceGetHandleByIndex = func(i uint64) (uintptr, error) {
 		var device uintptr
-		rc, _, _ := resolved[symbolNvmlDeviceGetHandleByIndex_v2].Call(uintptr(i), uintptr(unsafe.Pointer(&device)))
+		rc, _, _ := getHandleFunc.Call(uintptr(i), uintptr(unsafe.Pointer(&device)))
 		if rc := nvmlError(rc); rc != NVML_SUCCESS {
 			return 0, rc
 		}
 		return device, nil
 	}
+	getNameFunc := resolved[symbolNvmlDeviceGetName]
 	nvmlDeviceGetName = func(device uintptr) (string, error) {
-		var buf [96]byte 
-		rc, _, _ := resolved[symbolNvmlDeviceGetName].Call(device, uintptr(unsafe.Pointer(&buf[0])), uintptr(len(buf)))
+		var buf [96]byte
+		rc, _, _ := getNameFunc.Call(device, uintptr(unsafe.Pointer(&buf[0])), uintptr(len(buf)))
 		if rc := nvmlError(rc); rc != NVML_SUCCESS {
 			return "", rc
 		}
 		return strings.ReplaceAll(string(buf[:]), "\000", ""), nil
 	}
+	getPowerFunc := resolved[symbolNvmlDeviceGetPowerUsage]
 	nvmlDeviceGetPowerUsage = func(device uintptr) (uint32, error) {
 		var mW uint32
-		rc, _, _ := resolved[symbolNvmlDeviceGetPowerUsage].Call(device, uintptr(unsafe.Pointer(&mW)))
+		rc, _, _ := getPowerFunc.Call(device, uintptr(unsafe.Pointer(&mW)))
 		if rc := nvmlError(rc); rc != NVML_SUCCESS {
 			return 0, rc
 		}
 		return mW, nil
 	}
 	// Optional symbols
-	if f, ok := resolved[symbolNvmlDeviceGetArchitecture]; ok {
+	if getArchFunc, ok := resolved[symbolNvmlDeviceGetArchitecture]; ok {
 		nvmlDeviceGetArchitecture = func(device uintptr) (nvmlDeviceArchitecture, error) {
 			var arch nvmlDeviceArchitecture
-			rc, _, _ := f.Call(device, uintptr(unsafe.Pointer(&arch)))
+			rc, _, _ := getArchFunc.Call(device, uintptr(unsafe.Pointer(&arch)))
 			if rc := nvmlError(rc); rc != NVML_SUCCESS {
 				return 0, rc
 			}
 			return arch, nil
 		}
 	}
-	if f, ok := resolved[symbolNvmlDeviceGetTotalEnergyConsumption]; ok {
+	if getEnergyFunc, ok := resolved[symbolNvmlDeviceGetTotalEnergyConsumption]; ok {
 		nvmlDeviceGetTotalEnergyConsumption = func(device uintptr) (uint64, error) {
 			var uJ uint64
-			rc, _, _ := f.Call(device, uintptr(unsafe.Pointer(&uJ)))
+			rc, _, _ := getEnergyFunc.Call(device, uintptr(unsafe.Pointer(&uJ)))
 			if rc := nvmlError(rc); rc != NVML_SUCCESS {
 				return 0, rc
 			}
